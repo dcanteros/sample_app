e655ab3010301047e27c30747c0da29a4fc02304
David G. Canteros <dcanteros@gmail.com>
2010-06-23 12:18:21 -0300

    Finished first cut of the User Model

--- spec/integration/layout_links_spec.rb
+++ spec/integration/layout_links_spec.rb
@@ -1,4 +1,10 @@
 require 'spec_helper'
+require "webrat"
+
+Webrat.configure do |config|
+   config.mode = :rails
+end
+
 
 describe "LayoutLinks" do
 
@@ -25,6 +31,20 @@ describe "LayoutLinks" do
   it "Should have a signup page at '/signup'" do
      get '/signup'
      response.should render_template 'users/new'
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    response.should render_template 'pages/about'
+    click_link "Help"
+    response.should render_template 'pages/help'
+    click_link "Contact"
+    response.should render_template 'pages/contact'
+    click_link "Home"
+    response.should render_template 'pages/home'
+    click_link "Sign up now!"
+    response.should render_template 'users/new'
   end
 end
 
